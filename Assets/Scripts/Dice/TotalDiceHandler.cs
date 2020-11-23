33113f8d84f0a1c258f2c47eaa0ca6fd84b17aff
marshall brock <marshallbrock@gmail.com>
2020-11-23 14:21:31 -0600

    scoring buttons have false points but i believe rerender like they should

--- Assets/Scripts/Dice/TotalDiceHandler.cs
+++ Assets/Scripts/Dice/TotalDiceHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TotalDiceHandler : MonoBehaviour
 {
@@ -19,6 +20,16 @@ public class TotalDiceHandler : MonoBehaviour
 
     [SerializeField] GameObject buttonHander;
     [SerializeField] GameObject scoreRollButton;
+
+    public GameObject buttonParent;
+    public GameObject buttonOne;
+    public GameObject buttonTwo;
+    public GameObject buttonThree;
+    public GameObject buttonFour;
+    public GameObject buttonFive;
+    public GameObject buttonSix;
+
+    public int activeButtonCount = 0;
 
     public Display displayPlayerChanger;
 
@@ -82,6 +93,7 @@ public class TotalDiceHandler : MonoBehaviour
     }
     public void Score()
     {
+        Debug.Log("RUNNING SCORE");  
         //create buttons for each possible score starting from highest to lowest
         //each button should add the score they have
         //reset the values that it takes from the dice
@@ -95,171 +107,92 @@ public class TotalDiceHandler : MonoBehaviour
         //in game over i need to set each child of what is holding the dice to true so that all the dice are on and
         //i need to change the player
 
-        if(one > 1)
+        //buttons parent =
+        //if buttons parent.childcount is less then 6
+
+        //deactivate button parents children-- no deactive all children when you press the buttn
+        //deactive childs
+        //for (int i = 0; i < buttonParent.transform.childCount; i++)
+        //{
+        //    var child = buttonParent.transform.GetChild(i).gameObject;
+        //    child.SetActive(false);
+        //}
+        //im deactivating all buttons except specific nuttons
+        //
+        //buttonParent.transform.childCount < 3 is getting all the children not the active ones
+        //IT MIGHT WOEK BETTER IF THE BUTTONS WERE INSTANTIATED RATHER THAN JUST ACTIVE
+        if (one >= 1 && activeButtonCount < 6)
         {
-
+            //I NEED TO SUBTRACT ONE BUT I ALSO NEED TO FIGURE OUT HOW TO KEEP THE NUMBER SO IF I DONT CHOOSE THE BUTTON IT WILL ADD THEM BACK
+            Debug.Log("BUTTON 1");//activate button
+            buttonOne.SetActive(true);
+            //one--;//this is to make sure it decreases so when it runs again it won't show up.
+            //THIS PROBLEM WITH THIS IS WITHOUT SUBRACTING THE  VAR IT WILL KEEP RUNNING SCORE AND RENDER ONE UNTIL ACTIVE BUTTON COUNTIS FALSE
+            score++;//i want to increase the score. but i dont think this is necesarry/ this is so i dont get a game over right now
+            activeButtonCount++;
+            //one -= 1;//this should be on the button
+            //Score();//call the same function
+            //return;//return out so you dont run the rest.. I HAD THIS RETURN SO IT DIDNT RUN THE GAME OVER FUNCTION or the ROUND OVER UNTIL EVRYTHING WAS DONE
         }
-        if (two > 1)
+        if (two >= 1 && activeButtonCount < 3)
         {
-
+            Debug.Log("BUTTON 2");
+            buttonTwo.SetActive(true);
+            //two--;
+            score++;
+            activeButtonCount++;
+            //Score();//call the same function
+            //return;//return out so you dont run the rest
         }
-        if (three > 1)
+        if (three >= 1 && activeButtonCount < 3)
         {
-
+            Debug.Log("BUTTON 3");
+            buttonThree.SetActive(true);
+            //three--;
+            score++;
+            activeButtonCount++;
+            //Score();//call the same function
+            //return;//return out so you dont run the rest
+        }
+        if (four >= 1 && activeButtonCount < 3)
+        {
+            Debug.Log("BUTTON 4");
+            buttonFour.SetActive(true);
+            //four--;
+            score++;
+            activeButtonCount++;
+            //Score();//call the same function
+            //return;//return out so you dont run the rest
+        }
+        if (five >= 1 && activeButtonCount < 3)
+        {
+            Debug.Log("BUTTON 5");
+            buttonFive.SetActive(true);
+            //five--;
+            score++;
+            activeButtonCount++;
+            //Score();//call the same function
+            //return;//return out so you dont run the rest
+        }
+        if (six >= 1 && activeButtonCount < 3)
+        {
+            Debug.Log("BUTTON 6");
+            buttonSix.SetActive(true);
+            //six--;
+            score++;
+            activeButtonCount++;
+            //Score();//call the same function
+            //return;//return out so you dont run the rest
         }
 
-        //switch (six)
-        //{
-        //    case 6:
-        //        score += 3000;
-        //        break;
-        //    case 5:
-        //        score += 2000;
-        //        break;
-        //    case 4:
-        //        score += 1000;
-        //        break;
-        //    case 3:
-        //        score += 600;
-        //        break;
-        //    default:
-        //        score += 0;
-        //        break;
-        //}
-        //switch (five)
-        //{
-        //    case 6:
-        //        score += 3000;
-        //        five -= 6;
-        //        break;
-        //    case 5:
-        //        score += 2000;
-        //        break;
-        //    case 4:
-        //        score += 1000;
-        //        break;
-        //    case 3:
-        //        score += 500;
-        //        break;
-        //    case 2:
-        //        score += 100;
-        //        break;
-        //    case 1:
-        //        score += 50;
-        //        break;
-        //    default:
-        //        score += 0;
-        //        break;
-        //}
-        //switch (four)
-        //{
-        //    case 6:
-        //        score += 3000;
-        //        break;
-        //    case 5:
-        //        score += 2000;
-        //        break;
-        //    case 4:
-        //        score += 1000;
-        //        break;
-        //    case 3:
-        //        score += 400;
-        //        break;
-        //    default:
-        //        score += 0;
-        //        break;
-        //}
-        //switch (three)
-        //{
-        //    case 6:
-        //        score += 3000;
-        //        three -= 6;
-        //        break;
-        //    case 5:
-        //        score += 2000;
-        //        three -= 5;
-        //        break;
-        //    case 4:
-        //        score += 1000;
-        //        three -= 4;
-        //        break;
-        //    case 3:
-        //        score += 300;
-        //        three -= 3;
-        //        break;
-        //    default:
-        //        score += 0;
-        //        break;
-        //}
-        //switch (two)
-        //{
-        //    case 6:
-        //        score += 3000;
-        //        two -= 6;
-        //        break;
-        //    case 5:
-        //        score += 2000;
-        //        two -= 5;
-        //        break;
-        //    case 4:
-        //        score += 1000;
-        //        two -= 4;
-        //        break;
-        //    case 3:
-        //        score += 200;
-        //        two -= 3;
-        //        break;
-        //    default:
-        //        score += 0;
-        //        break;
-        //}
-        //switch (one)
-        //{
-        //    case 6:
-        //        score += 3000;
-        //        one -= 6;//insurance so i dont count the roll twice
-        //        break;
-        //    case 5:
-        //        score += 2000;
-        //        one -= 5;
-        //        break;
-        //    case 4:
-        //        score += 1000;
-        //        one -= 4;
-        //        break;
-        //    case 3:
-        //        score += 300;
-        //        one -= 3;
-        //        break;
-        //    case 2:
-        //        score += 200;
-        //        one -= 2;
-        //        break;
-        //    case 1:
-        //        score += 100;
-        //        one -= 1;
-        //        break;
-        //    default:
-        //        score += 0;
-        //        break;
-        //}
-
-        ////one of all kinds
-        //if ((one.ToString() == "1") && (two.ToString() == "1") && (three.ToString() == "1") && (four.ToString() == "1") && (five.ToString() == "1") && (six.ToString() == "1"))
-        //{
-        //    score += 1500;
-        //}
-
-        ////three pairs
-
-        if (score > 0) { 
+        if (score > 0 && activeButtonCount == 0) { 
         //if score is greater than 0 do this 
         Debug.Log("YOURE SCORE THIS ROUND IS " + score);
         totalScore += score;
         Debug.Log("YOURE TOTAL SCORE IS" + totalScore);
-        ScoreReset();
+        //ScoreReset();
         }
-        else
+        else if(score <= 0 && activeButtonCount == 0)
         {
             Debug.Log("GAME OVER");
             //be sure to make sure to make a function that will skip the player if he is out
@@ -271,6 +204,36 @@ public class TotalDiceHandler : MonoBehaviour
 
         }
         
+    }
+
+    //i dont think i need this. I might but i think
+    //i might just make the numbers public
+    public void TakeAwayDice(int dieNumber)
+    {
+        switch (dieNumber)
+        {
+            case 6:
+                six -= 1;
+                break;
+            case 5:
+                five -= 1;
+                break;
+            case 4:
+                four -= 1;
+                break;
+            case 3:
+                three -= 1;
+                break;
+            case 2:
+                two -= 1;
+                break;
+            case 1:
+                one -= 1;
+                break;
+            default:
+                Debug.Log("Danger");
+                break;
+        }
     }
 
 
@@ -291,6 +254,8 @@ public class TotalDiceHandler : MonoBehaviour
         Debug.Log("YOURE SCORE Reset To  " + 0);
         score = 0;
     }
+
+
 
 }
 //3 scores
---
+++ Assets/Scripts/UI/ButtonScoreList.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScoreList : MonoBehaviour
+{
+
+    public GameObject buttonParent;
+    public GameObject buttonOne;
+    public GameObject buttonTwo;
+    public GameObject buttonThree;
+    public GameObject buttonFour;
+    public GameObject buttonFive;
+    public GameObject buttonSix;
+
+    public TotalDiceHandler totalDiceHandler;
+
+    public void ButtonOne()
+    {
+        Debug.Log("ACTIVE BUTTON COUNT " + totalDiceHandler.activeButtonCount);
+
+        //add thescore
+        //deactivate all the buttons
+        DeactivateButtons();//deactivates all buttons
+        totalDiceHandler.TakeAwayDice(1);//take away the correct number of dice you're choosing
+        totalDiceHandler.activeButtonCount = 0;//zeroes out which buttons are active
+        totalDiceHandler.Score();//rerencer the buttons
+    }
+    public void ButtonTwo()
+    {
+
+    }
+
+    public void ButtonThree()
+    {
+
+    }
+    public void ButtonFour()
+    {
+
+    }
+    public void ButtonFive()
+    {
+
+    }
+    public void ButtonSix()
+    {
+
+    }
+
+    public void DeactivateButtons()
+    {
+        for (int i = 0; i < buttonParent.transform.childCount; i++)
+        {
+            var child = buttonParent.transform.GetChild(i).gameObject;
+            child.SetActive(false);
+            //if (child.transform.GetSiblingIndex() != indexOfCurrentButton)
+            //{
+            //    child.SetActive(false);
+            //}
+        }
+    }
+
+}
