70f7e77b35f4a93f46e88849cbe63409a29dd911
Marshall Brock <marshallbrock@Marshalls-Air.attlocal.net>
2021-10-23 12:01:59 -0500

    switching

---
+++ Assets/Tinder Swipe/GameManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    private PlayerData player;
+    public List<PlayerData> totalPlayers;
+    public static GameData data;
+
+    public void Awake()
+    {
+        data = new GameData();
+    }
+    
+
+ 
+}
